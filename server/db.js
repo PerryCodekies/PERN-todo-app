57a87424580951b45cd89c2447136d48c1ba52af
PerryCodekies <15182814+PerryCodekies@users.noreply.github.com>
2021-05-11 15:17:23 +0800

    Removed sensitive data

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    ***REMOVED***: "***REMOVED***",
+    password: "password",
     host: "localhost",
     port: 5432,
     database: "perntodo"
